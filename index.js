e458c7067c50d38a218124659628eaf29ec48eba
JatinMittal552 <jatinmittal552@gmail.com>
2024-10-30 23:04:29 +0530

    first

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const net = require("net");
-const { connected } = require("process");
 
 console.log("Logs from your program will appear here!");
 
@@ -7,10 +6,11 @@ const server = net.createServer((connection) => {
     
     connection.addListener('data',(data)=>{
         const request = data.toString();
+
         if(request=="*1\r\n$4\r\nping\r\n"){
             connection.write('+PONG\r\n');
             connection.end();
         }
     })
 });
-server.listen(6379, "127.0.0.1");
+server.listen(8000, "127.0.0.1");
